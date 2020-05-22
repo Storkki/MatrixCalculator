24de89e6be65f1ccb0d90eafa95a25bc9408ae00
Silqunz <silqunzv2@gmail.com>
2020-05-23 02:58:39 +0300

    Replaced Express.js with native http

--- app.js
+++ app.js
@@ -1,24 +1,76 @@
 'use strict'
 
-const express = require('express');
+const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
+const hostname = '127.0.0.1';
 const port = 3000;
-const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+const fileType = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    // '.json': 'application/json',
+    // '.ico': 'image/x-icon',
+    // '.png': 'image/png',
+};
 
-app.get('/', (req, res) => {
-    res.redirect('/matrices');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 302; //redirect
+        res.setHeader('Location', '/matrices')
+        res.end();
+        return;
+    }
+
+    let page = req.url;
+    let pathname;
+    let ext = path.extname(req.url);
+
+    if(req.url === '/matrices') {
+        page = '/matrix.html';
+        ext = '.html';
+    }
+
+    if(req.url === '/graphs') {
+        page = '/graph.html';
+        ext = '.html';
+    }
+
+    if (fileType[ext] === 'text/html') {
+        pathname = path.join(__dirname, 'public', 'pages', page);
+    }else if (fileType[ext] === 'text/css') {
+        pathname = path.join(__dirname, 'public', page);
+    }else if (fileType[ext] === 'text/javascript') {
+        pathname = path.join(__dirname, 'public', page);
+    }
+
+    const info = {
+        Type: ext,
+        Path: pathname
+    };
+    console.dir(info)
+
+    fs.exists(pathname, function (exist) {
+        if(!exist) {
+            res.statusCode = 404;
+            res.end(`File ${pathname} not found!`);
+            return;
+        }
+
+        fs.readFile(pathname, function(err, data){
+            if(err){
+                res.statusCode = 500;
+                res.end(`Error getting the file: ${err}.`);
+            } else {
+                res.setHeader('Content-type', fileType[ext] || 'text/plain' );
+                res.end(data);
+            }
+        });
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Starting on ${hostname}:${port}`)
 })
-
-app.get('/matrices', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'pages', 'matrix.html'));
-})
-
-
-app.get('/graphs', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'pages', 'graph.html'));
-})
-
-app.listen(port, () => console.log(`Starting on ${port}`));
